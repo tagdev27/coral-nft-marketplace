aad06d18cfee77340c86c73f0a5b6a8fbacc28ad
TAGDEV TECHNOLOGIES LIMITED <51853262+tagdev27@users.noreply.github.com>
2021-12-15 14:22:46 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "_next/**/*.{js,ts,jsx,tsx}"
+    './layout/**/*.{js,ts,jsx,tsx}',
+    './helpers/**/*.{js,ts,jsx,tsx}'
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
